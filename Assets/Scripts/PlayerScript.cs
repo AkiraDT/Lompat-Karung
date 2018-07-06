7cc63a0fdc12818fd26ee30a4eb65e8466f7fb35
Dennis Indrawan <dennisindra22@gmail.com>
2018-07-07 03:29:48 +0900

    add jump mechanic

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	private bool onGround;
+
+
+	// Use this for initialization
+	void Start () {
+		onGround = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.CompareTag ("Ground")) {
+			onGround = true;
+		}
+	}
+
+
+	public bool OnGround{
+		get{
+			return onGround;
+		}
+		set{ 
+			onGround = value;
+		}
+	}
+}
---
+++ Assets/Scripts/TouchInputMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TouchInputMovement : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+	//Untuk mengontrol player
+	GameObject Player;
+	private float jumpPressure;
+	private float minJump;
+	private float maxJumpPressure;
+	private Rigidbody2D rb;
+	private bool hold;
+
+	// Use this for initialization
+	void Start () {
+		hold = false;
+		Player = GameObject.Find ("Player");
+		rb = Player.GetComponent<Rigidbody2D> ();
+		minJump = 2f;
+		jumpPressure = 0f;
+		maxJumpPressure = 10f;
+	}
+
+	void Update(){
+		if (hold) {
+			if (Player.GetComponent<PlayerScript> ().OnGround) {
+				//if (jumpPressure < maxJumpPressure) {
+				jumpPressure += Time.deltaTime * 10f;
+				//} else {
+				//jumpPressure = maxJumpPressure;
+				//}
+			}
+		}
+	}
+
+	//ketika menyentuh layar
+	public virtual void OnPointerDown(PointerEventData ped){
+		hold = true;
+	}
+
+	//ketika sentuhan dilepaskan
+	public virtual void OnPointerUp(PointerEventData ped){
+		hold = false;
+		if (jumpPressure > 0) {
+			jumpPressure = jumpPressure + minJump;
+			rb.velocity = new Vector2 (jumpPressure/2f, jumpPressure);
+			jumpPressure = 0;
+			Player.GetComponent<PlayerScript> ().OnGround = false;
+		}
+	}
+}
