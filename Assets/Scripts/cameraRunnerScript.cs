cba30ee9235d2955fa3ebceecb149485ef11ef53
Dennis Indrawan <dennisindra22@gmail.com>
2018-07-10 13:10:56 +0900

    add background scrolling

--- Assets/Scripts/cameraRunnerScript.cs
+++ Assets/Scripts/cameraRunnerScript.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class cameraRunnerScript : MonoBehaviour {
 
 	public Transform player;
-	
+
+	void Start(){
+		Screen.orientation = ScreenOrientation.Landscape;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3 (player.position.x + 6, 0, -10);
