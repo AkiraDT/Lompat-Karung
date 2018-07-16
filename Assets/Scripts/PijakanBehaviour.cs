6118cc558cf71188efe713983963790484112c86
Dennis Indrawan <dennisindra22@gmail.com>
2018-07-17 08:07:25 +0900

    camera doesn't move when jumping

--- Assets/Scripts/PijakanBehaviour.cs
+++ Assets/Scripts/PijakanBehaviour.cs
@@ -3,22 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PijakanBehaviour : MonoBehaviour {
-	/*
-	private Transform par;
-	// Use this for initialization
-	void Start () {
-		par = transform.parent.transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private bool firstLand;
+
+	void Start(){
+		firstLand = true;
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
-		if (col.gameObject.CompareTag ("Ground")) {
-			par.position = new Vector2 (par.position.x+5, par.position.y);
+		if (col.gameObject.CompareTag ("Player")) {
+			if (firstLand) {
+				firstLand = false;
+
+			}
 		}
 	}
-	*/
+
 }
--- Assets/Scripts/cameraRunnerScript.cs
+++ Assets/Scripts/cameraRunnerScript.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class cameraRunnerScript : MonoBehaviour {
 
 	public Transform player;
+	private PlayerScript PS;
+	private Rigidbody2D rb;
 
 	void Start(){
 		Screen.orientation = ScreenOrientation.Landscape;
+		PS = GameObject.FindObjectOfType<PlayerScript>();
+		rb = GetComponent<Rigidbody2D> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3 (player.position.x + 6, 0, -10);
+		if (!PS.OnGround) {
+			rb.velocity = new Vector2 (GameControlScript.Instance.scrollSpeed, 0f);
+		} else {
+			rb.velocity = Vector2.zero;
+			if (transform.position.x <= player.position.x + 6) {
+				transform.Translate (Vector3.right * 0.1f);
+			}
+		}
 	}
 }
